4d181971b4e5f0db0f02ee1e70cd2c4450509035
Gerard4168 <gerard2234506@correo.uis.edu.co>
2023-03-11 10:55:38 -0500

    subir

---
+++ proceso.py
@@ -0,0 +1,25 @@
+# Programa para saber el numero opuesto de un dado
+
+# inpur
+
+N_Dado = int (input(" Ingrese en valor: "))
+
+
+# proceso
+
+       
+if (N_Dado == 6):
+    print(" Su inverso es: 1")
+elif (N_Dado == 5):
+    print(" Su inverso es: 2")
+elif (N_Dado == 4):
+    print(" Su inverso es: 3")
+elif (N_Dado == 3):
+    print(" Su inverso es: 4")
+elif (N_Dado == 2):
+    print(" Su inverso es: 5")
+elif (N_Dado == 1):
+    print(" Su inverso es: 6")
+else: 
+     print("Ingrese un valor entre 1 y 6")
+
